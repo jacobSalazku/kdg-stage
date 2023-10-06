162d28cd3724899b2384db5de693e38c9cbd0a65
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-06 18:19:03 +0200

    feat(register): Add first name, last name, phone number, and company fields to registration form

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
+        'phone_number',
+        'company',
         'password',
     ];
 

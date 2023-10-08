2c0a05cf7248c56426c1f15e82566a366b71580b
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-08 11:09:36 +0200

    feat: Add new internship link to responsive navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,6 +76,12 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('new')" :active="request()->routeIs('new')">
+                {{ __('New Internship') }}
+            </x-responsive-nav-link>
+        </div>
+
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">

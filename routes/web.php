c7c53dad805713944408875446d4b28e5816542d
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-08 11:03:03 +0200

    feat: Add new route for creating a new internship

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/new')->name('new');
 });
 
 require __DIR__.'/auth.php';

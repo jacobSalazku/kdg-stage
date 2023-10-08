22166d39c9031bc5884a5e9477500c9aa2ba94b5
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-08 14:24:27 +0200

    fix: Update heading text in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">Your Internships</h5>
+                    <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">Internships</h5>
                     @foreach($internships as $internship)
                         <a href="#" class="mt-4 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$internship->title}} - {{$internship->user->company}}</h5>

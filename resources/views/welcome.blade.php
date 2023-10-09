8e8f4df15337e2319818627aab1cc35960efaf61
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-09 09:12:14 +0200

    style: Add date and time to internship card in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
                         <a href="{{route('detail', ['id' => $internship->id])}}" class="mt-4 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$internship->title}} - {{$internship->user->company}}</h5>
                             <p class="font-normal text-gray-700 dark:text-gray-400">{{$internship->description}}</p>
+                            <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Posted: {{$internship->updated_at->format('d-m-y h:i')}}</p>
                         </a>
                     @endforeach
                     <br>

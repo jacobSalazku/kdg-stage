1f097a1d0d4a72190797d6a1786aca600827104b
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-09 10:27:13 +0200

    feat(internship): Add soft deletes functionality to internship model and internships table

--- database/migrations/2023_10_06_153231_create_internships_table.php
+++ database/migrations/2023_10_06_153231_create_internships_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->text('description');
             $table->string('website');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

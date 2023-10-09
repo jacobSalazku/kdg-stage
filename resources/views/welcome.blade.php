8d2c203757770e943b434cbc75d5b2ec2a38ad14
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-09 12:36:27 +0200

    chore: Simplify search form in welcome.blade.php and add "Go back" button when filtered

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,19 @@
             <div class="flex-shrink-0 flex items-center">
                 <form action="{{route('search')}}" method="get">
                     <div class="relative flex items-center">
-                        <input type="text" name="query" id="query" placeholder="Find Internships" class="block w-64 py-2 pl-10 pr-4 leading-5 rounded-full bg-gray-100 dark:bg-gray-700 border-transparent focus:outline-none focus:bg-white focus:border-gray-300 dark:focus:border-gray-500 focus:ring focus:ring-gray-200 dark:focus:ring-gray-400 transition duration-150 ease-in-out">
-                        <button type="submit" class="ml-2 px-3 py-2 rounded-full bg-gray-100 dark:bg-gray-700 text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-200 dark:focus:bg-gray-600 transition duration-150 ease-in-out">
-                            Search
-                        </button>
+                        <form action="{{route('search')}}" method="get">
+                            <input type="text" name="query" id="query" placeholder="Find Internships" class="block w-64 py-2 pl-10 pr-4 leading-5 rounded-full bg-gray-100 dark:bg-gray-700 border-transparent focus:outline-none focus:bg-white focus:border-gray-300 dark:focus:border-gray-500 focus:ring focus:ring-gray-200 dark:focus:ring-gray-400 transition duration-150 ease-in-out">
+                            <button type="submit" class="ml-2 px-3 py-2 rounded-full bg-gray-100 dark:bg-gray-700 text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-200 dark:focus:bg-gray-600 transition duration-150 ease-in-out">
+                                Search
+                            </button>
+                            @if($filtered === 1)
+                                <button class="ml-2 px-3 py-2 rounded-full bg-gray-100 dark:bg-gray-700 text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-200 dark:focus:bg-gray-600 transition duration-150 ease-in-out">
+                                    <a href="{{route('home')}}">Go back</a>
+                                </button>
+                            @endif
                     </div>
                 </form>
             </div>
-
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-4">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @if ($filtered === 1)

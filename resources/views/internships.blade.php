fa297f676e90a78021474f552cec65d57e7338e5
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-10 11:48:32 +0200

    Refactor internships.blade.php view file layout

--- resources/views/internships.blade.php
+++ resources/views/internships.blade.php
@@ -5,10 +5,12 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">Internships</h5>
                     @foreach($companies as $company)
-                        <a href="" class="mt-4 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full">
+                        <div class="mt-4 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$company->company}}</h5>
+                            <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Email: <a href="mailto:{{$company->email}}">{{$company->email}}</a></p>
+                            <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Phone: <a href="Tel:{{$company->phone_number}}">{{$company->phone_number}}</a></p>
                             <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Registered: {{$company->created_at->format('d-m-y h:i')}}</p>
-                        </a>
+                        </div>
                     @endforeach
                 </div>
             </div>

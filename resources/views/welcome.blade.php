0e0e17d5f8c891b609d33a42610ba3ba79a99122
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-10 09:15:39 +0200

    Refactor block width and add "w-full" class to anchor tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
                         <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">Internships</h5>
                     @endif
                     @foreach($internships as $internship)
-                        <a href="{{route('detail', ['id' => $internship->id])}}" class="mt-4 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <a href="{{route('detail', ['id' => $internship->id])}}" class="mt-4 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$internship->title}} - {{$internship->user->company}}</h5>
                             <p class="font-normal text-gray-700 dark:text-gray-400">{{substr($internship->description, 0, 305)}} ...</p>
                             <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Posted: {{$internship->updated_at->format('d-m-y h:i')}}</p>

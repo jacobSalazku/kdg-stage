5a50731f2395260cd0615e6335f1b4e28ff40412
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-13 11:43:36 +0200

    docs: Add website link for company in internships view

--- resources/views/internships.blade.php
+++ resources/views/internships.blade.php
@@ -9,6 +9,7 @@
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$company->company}}</h5>
                             <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Email: <a href="mailto:{{$company->email}}">{{$company->email}}</a></p>
                             <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Phone: <a href="Tel:{{$company->phone_number}}">{{$company->phone_number}}</a></p>
+                            <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Website: <a target="__blank" href="{{$company->website}}">{{$company->website}}</a></p>
                             <p class="mt-4 text-sm text-gray-700 dark:text-gray-400">Registered: {{$company->created_at->format('d-m-y H:i')}}</p>
                         </div>
                     @endforeach

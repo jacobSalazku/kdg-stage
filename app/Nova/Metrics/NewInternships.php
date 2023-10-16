031dd10ac5a4cee035deadccac6cbe4b9a17afe3
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-16 15:44:58 +0200

    feat: Add new Nova metrics for new internships, new jobs, and new users

---
+++ app/Nova/Metrics/NewInternships.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use App\Models\Internship;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Value;
+
+class NewInternships extends Value
+{
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+        return $this->count($request, Internship::class);
+    }
+
+    /**
+     * Get the ranges available for the metric.
+     *
+     * @return array
+     */
+    public function ranges()
+    {
+        return [
+            30 => '30 Days',
+            60 => '60 Days',
+            365 => '365 Days',
+            'TODAY' => 'Today',
+            'YESTERDAY' => 'Yesterday',
+            'MTD' => 'Month To Date',
+            'QTD' => 'Quarter To Date',
+            'YTD' => 'Year To Date',
+        ];
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'new-internships';
+    }
+}

08f475c48aa702944a429035cce5995f978bc261
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-16 11:08:14 +0200

    style: Add toolbar options to Quill editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,25 @@
         </div>
         <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
         <script>
+            var toolbarOptions = [
+                ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+                ['blockquote', 'code-block'],
+
+                [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+                [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+                [{ 'header': [1, 2, 3, 4, 5, 6, false] }],       // dropdown with defaults from theme
+                [{ 'align': [] }],
+
+                ['clean']                                         // remove formatting button
+            ];
+
             var quill = new Quill('#editor', {
+                modules: {
+                    toolbar: toolbarOptions
+                },
                 theme: 'snow'
             });
 

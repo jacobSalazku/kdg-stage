d0ba4bd386917bcee14afb764191671511aaa663
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-16 17:13:33 +0200

    feat(tag): Add TagController and Tag model, with relationships to Internship model

--- app/Models/Internship.php
+++ app/Models/Internship.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Internship extends Model
 {
@@ -20,5 +21,10 @@ class Internship extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }

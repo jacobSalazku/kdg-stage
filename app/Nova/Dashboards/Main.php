02e2c53c4ecfadce7357e3f241b010469027ce0a
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-16 15:45:09 +0200

    feat: Add new metrics to main dashboard

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,7 +2,9 @@
 
 namespace App\Nova\Dashboards;
 
-use Laravel\Nova\Cards\Help;
+use App\Nova\Metrics\NewInternships;
+use App\Nova\Metrics\NewUsers;
+use App\Nova\Metrics\NewJobs;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class Main extends Dashboard
@@ -15,7 +17,9 @@ class Main extends Dashboard
     public function cards()
     {
         return [
-            new Help,
+            new NewUsers,
+            new NewJobs,
+            new NewInternships,
         ];
     }
 }

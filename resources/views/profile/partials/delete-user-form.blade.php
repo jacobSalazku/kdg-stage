d183b4be15512627f868e22b39df1f188507cce5
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-18 14:47:53 +0200

    refactor(profile): Remove update-password-form and unused code

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -5,7 +5,7 @@
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted.') }}
         </p>
     </header>
 
@@ -28,17 +28,17 @@
             </p>
 
             <div class="mt-6">
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+                <x-input-label for="email" value="{{ __('Email') }}" class="sr-only" />
 
                 <x-text-input
-                    id="password"
-                    name="password"
-                    type="password"
+                    id="email"
+                    name="email"
+                    type="email"
                     class="mt-1 block w-3/4"
-                    placeholder="{{ __('Password') }}"
+                    placeholder="{{ __('Email') }}"
                 />
 
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->userDeletion->get('email')" class="mt-2" />
             </div>
 
             <div class="mt-6 flex justify-end">

ac430012a69ef451d5a7be79afadf8817260ed56
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-25 16:33:41 +0200

    feat(lang): add login page language translations for English and Dutch

---
+++ lang/nl/login.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for the login page
+    |
+    */
+
+    'microsoft' => 'Inloggen met microsoft',
+    'github' => 'Inloggen met github',
+    'google' => 'Inloggen met google',
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,12 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <x-primary-button class="w-full">
-        <a href="{{ route('oauth.login', [ 'provider' => 'microsoft' ]) }}" class="mx-auto">Log in with Microsoft</a>
+        <a href="{{ route('oauth.login', [ 'provider' => 'microsoft' ]) }}" class="mx-auto">{{__('login.microsoft')}}</a>
     </x-primary-button>
     <x-primary-button class="w-full mt-4">
-        <a href="{{ route('oauth.login', [ 'provider' => 'github' ]) }}" class="mx-auto">Log in with GitHub</a>
+        <a href="{{ route('oauth.login', [ 'provider' => 'github' ]) }}" class="mx-auto">{{__('login.github')}}</a>
     </x-primary-button>
     <x-primary-button class="w-full mt-4">
-        <a href="{{ route('oauth.login', [ 'provider' => 'google' ]) }}" class="mx-auto">Log in with Google</a>
+        <a href="{{ route('oauth.login', [ 'provider' => 'google' ]) }}" class="mx-auto">{{__('login.google')}}</a>
     </x-primary-button>
 </x-guest-layout>

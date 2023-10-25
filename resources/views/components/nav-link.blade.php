f9a61a63ff16f3ed649e01c910bf7c739a2c90ab
jacobSalazku <Cooljss10@gmail.com>
2023-10-26 01:07:45 +0200

    creating mobile menu

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+            ? ' text-kdg-white text-md font-semibold hover:bg-p-black hover:text-kdg-white px-4 py-8'
+            :' transition delay-50 ease-out text-kdg-white hover:bg-p-black hover:text-kdg-white px-4 py-8' ;
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>

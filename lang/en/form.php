f4d62d4e625ab3917550859250b99a2f484b4917
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-10-25 16:09:27 +0200

    feat(form): add form language lines for English and Dutch

---
+++ lang/en/form.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various forms
+    |
+    */
+
+    'title' => 'Title',
+    'description' => 'Description',
+    'company' => 'Company',
+    'phone' => 'Phone Number',
+    'offer' => 'Offer internships',
+    'name' => 'Name',
+    'website' => 'Website URL',
+    'email' => 'Email',
+    'save' => 'save',
+    'create' => 'create',
+    'update' => 'update',
+    'delete' => 'delete',
+    'delete_account' => 'delete account',
+    'warning_job' => 'Your job opening will be reviewed by us, before it becomes public.',
+    'warning_internship' => 'Your internship will be reviewed by us, before it becomes public.'
+
+];
---
+++ lang/nl/form.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various forms
+    |
+    */
+
+    'title' => 'Titel',
+    'description' => 'Omschrijving',
+    'company' => 'Bedrijf',
+    'phone' => 'Telefoonnummer',
+    'offer' => 'Stageplekken aanbieden',
+    'website' => 'Website URL',
+    'email' => 'Email',
+    'name' => 'naam',
+    'save' => 'opslaan',
+    'create' => 'aanmaken',
+    'update' => 'opslaan',
+    'delete' => 'verwijderen',
+    'delete_account' => 'account verwijderen',
+    'warning_job' => 'Uw vacature wordt door ons beoordeeld voordat deze openbaar wordt gemaakt.',
+    'warning_internship' => 'Uw stageplek wordt door ons beoordeeld voordat deze openbaar wordt.'
+];

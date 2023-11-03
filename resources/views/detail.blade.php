e1b3ff2991ba4c5d38a4b5e19d711a3d4dbefc9e
jacobSalazku <Cooljss10@gmail.com>
2023-11-03 20:17:52 +0100

    detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,21 +1,57 @@
-<x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @foreach($jobs as $job)
-                        <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">{{$job->title}}</h5>
-                        <h4 class="text-l font-semibold leading-none text-gray-800 dark:text-gray-200">{{$job->company}}</h4>
-                        <p class="text-sm mt-4 font-normal leading-none text-gray-800 dark:text-gray-200">{!! $job->description !!}</p>
-                        <p class="text-sm mt-4 font-normal leading-none text-gray-800 dark:text-gray-200">{{__('detail.posted')}} {{$job->updated_at->format('d-m-Y H:i')}}</p>
-                        <p class="text-sm mt-4 font-normal leading-none text-gray-800 dark:text-gray-200">{{__('detail.website')}} <a class="text-decoration-line: underline" target="__blank" href="{{$job->website}}">{{$job->website}}</a></p>
-                        <p class="text-sm mt-4 font-normal leading-none text-gray-800 dark:text-gray-200">{{__('detail.contact')}} {{$job->user->name}}</p>
-                        <p class="text-sm mt-4 font-normal leading-none text-gray-800 dark:text-gray-200">{{__('detail.email')}} <a class="text-decoration-line: underline" href="mailto:{{$job->email}}">{{$job->email}}</a></p>
-                        <p class="text-sm mt-4 font-normal leading-none text-gray-800 dark:text-gray-200">{{__('detail.phone')}} <a class="text-decoration-line: underline" href="Tel:">{{$job->phone_number}}</a></p>
-                    @endforeach
-                    <p class="text-l font-semibold mt-4"><a href="{{route('jobs')}}">{{__('detail.return')}}</a></p>
+ <x-app-layout>
+    @foreach($jobs as $job)
+        <div class="flex flex-col w-full h-full  items-center justify-start py-10 gap-6 bg-white">
+            <div class="  max-w-[87.5rem] flex flex-col items-center  justify-start gap-4">
+                <div class="border-b-2 border-kdg-light w-full h-full flex flex-row justify-center pb-4">
+                    <p class="text-3xl font-semibold">{{ $job->title }}</p>
+                </div>
+                <div>
+                    {{__('detail.posted')}} {{$job->updated_at->format('d-m-Y H:i')}}
+                </div>
+                <div class="  w-full flex flex-col justify-start px-2 py-6 ">
+                    <div class="flex flex-col sm:items-center">
+                        <div class=" border sm:w-auto sm:h-auto justify-start  rounded-md py-12  px-4 sm:mx-12 md:mx-24 md:px-8  lg:mx-8 lg:px-42 flex flex-col items-center">
+                            <p class="text-2xl font-semibold ">{{ $job->company }}</p>
+                            <div class="pt-10 text-lg">
+                                <p>{!! $job->description !!} </p>
+                            </div>
+                        </div>
+                        <div class=" w-full md:w-auto flex flex-col sm:flex-row justify-center items-center  border rounded-md py-5 my-5     ">
+
+                            <div class="flex  flex-col justify-center items-center pt-4 gap-4">
+                                <div>
+                                    <p class="text-lg font-semibold ">
+                                        Contant us
+                                    </p>
+                                </div>
+                                <div class="flex flex-col  md:flex-row gap-8 justify-center items-center">
+                                    <div class="flex  flex-row md:flex-col justify-center ">
+                                        <p> {{__('detail.phone')}} </p>
+                                        <p> {{ $job->phone_number}} </p>
+                                    </div>
+
+                                    <div class="flex flex-row sm:flex-col">
+                                        <p> {{__('detail.email')}} </p>
+                                        <p> {{ $job->email}} </p>
+                                    </div>
+                                    <div class="flex  flex-row sm:flex-col">
+                                        <p> {{__('detail.website')}} </p>
+                                        <p> <a class="text-decoration-line: underline" target="__blank" href="{{$job->website}}">{{$job->website}}</a> </p>
+                                    </div>
+
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <button class='flex w-auto justify-center   sm:mx-12 md:mx-24 mt-6 rounded-md bg-deep-black px-4   py-1.5   text-sm font-semibold leading-6 text-white shadow-sm hover:bg-kdg-dark-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"'>
+                                <p class="text-l font-semibold "><a href="{{route('jobs')}}">{{__('detail.return')}}</a></p>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endforeach
 </x-app-layout>
+

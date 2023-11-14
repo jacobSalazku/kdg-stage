391ea68fca426d4aea73870820d17b5ff0e0b95c
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-11-14 13:05:44 +0100

    refactor: Remove unused localization middleware and route group

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,31 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::group(['prefix' => LaravelLocalization::setLocale()], function()
-{
-    Route::get('/', [InternshipController::class, 'index'])->name('home');
-    Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
-    Route::get('/detail/{id}', [JobController::class, 'show'])->name('detail');
-    Route::get('/search', [JobController::class, 'index'])->name('search');
+Route::get('/', [InternshipController::class, 'index'])->name('home');
+Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
+Route::get('/detail/{id}', [JobController::class, 'show'])->name('detail');
+Route::get('/search', [JobController::class, 'index'])->name('search');
 
-    Route::get('oauth/login/{provider}', [oAuthController::class, 'redirectToProvider'])->name('oauth.login');
-    Route::get('oauth/callback/{provider}', [oAuthController::class, 'handleProviderCallback'])->name('oauth.callback');
+Route::get('oauth/login/{provider}', [oAuthController::class, 'redirectToProvider'])->name('oauth.login');
+Route::get('oauth/callback/{provider}', [oAuthController::class, 'handleProviderCallback'])->name('oauth.callback');
 
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        Route::get('/dashboard', [JobController::class, 'index'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [JobController::class, 'index'])->name('dashboard');
 
-        Route::get('/new', function(){
-            return view('new');
-        })->name('new');
-        Route::post('/new', [JobController::class, 'create'])->name('new');
+    Route::get('/new', function(){
+        return view('new');
+    })->name('new');
+    Route::post('/new', [JobController::class, 'create'])->name('new');
 
-        Route::get('/edit/{id}', [JobController::class, 'edit'])->name('edit');
-        Route::post('/edit/{id}', [JobController::class, 'update'])->name('update');
+    Route::get('/edit/{id}', [JobController::class, 'edit'])->name('edit');
+    Route::post('/edit/{id}', [JobController::class, 'update'])->name('update');
 
-        Route::post('/delete/{id}', [JobController::class, 'destroy'])->name('delete');
+    Route::post('/delete/{id}', [JobController::class, 'destroy'])->name('delete');
 
-        Route::post('/create-internship', [InternshipController::class, 'update'])->name('create_internship');
-    });
+    Route::post('/create-internship', [InternshipController::class, 'update'])->name('create_internship');
 });
 
 require __DIR__.'/auth.php';

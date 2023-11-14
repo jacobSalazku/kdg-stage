c2fb0e5c505ac0e1759fe9eafbf1d34ccb0bb772
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-11-14 13:17:59 +0100

    style: Remove unnecessary double quotation marks on button element

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="flex items-center justify-end mt-4">
-                            <button class="flex w-auto justify-center  mt-2 rounded-md bg-deep-black px-4   py-1.5  text-sm font-semibold leading-6 text-white shadow-sm hover:bg-kdg-dark-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"">
+                            <button class="flex w-auto justify-center  mt-2 rounded-md bg-deep-black px-4   py-1.5  text-sm font-semibold leading-6 text-white shadow-sm hover:bg-kdg-dark-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                 {{ __('form.create') }}
                             </button>
                         </div>

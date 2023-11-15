d41ca31090fabf026b079473ee9593c1b4198cb9
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-11-15 15:38:02 +0100

    Remove unnecessary lines in internships.blade.php

--- resources/views/internships.blade.php
+++ resources/views/internships.blade.php
@@ -7,7 +7,6 @@
                     {{__('internships.internships')}}
                     </h1>
                 </div>
-
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="ml-auto bg-light-blue  border-dark-blue border focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Filter <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
@@ -26,7 +25,6 @@
                 </div>
             </div>
         </div>
-
         <div class=" h-full  ">
             <div class=" w-full flex flex-row justify-center gap ">
                 <div class=" w-full h-full max-w-[87.5rem] flex flex-col  items-center sm:flex-row flex-wrap sm:justify-center gap-y-8 pt-20">

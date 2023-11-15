d6627a3884bd251027f2b8f6216f830f8bd98f75
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-11-15 15:13:45 +0100

    Refactor internships.blade.php view layout

--- resources/views/internships.blade.php
+++ resources/views/internships.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
     <div class=" w-full  h-full bg-kdg-white flex flex-col justify-center items-center pt-10 px-6">
         <div class="w-screen max-w-7xl flex flex-row items-center   ">
-            <div class="h-20 w-[87.5rem] flex flex-row justify-between border-b-2 border-kdg-light items-center px-10">
+            <div class="h-20 w-[87.5rem] flex flex-row border-b-2 border-kdg-light items-center px-10">
                 <div class="">
                     <h1 class=" text-2xl font-KDG sm:text-3xl">
                     {{__('internships.internships')}}
                     </h1>
                 </div>
 
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class=" bg-light-blue  border-dark-blue border focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Filter <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="ml-auto bg-light-blue  border-dark-blue border focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Filter <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
                 </button>
                 @if(request()->has('filter'))
-                    <a class="text-sm text-gray-700 dark:text-gray-400" href="{{route('home')}}">{{__('internships.filter')}}</a>
+                    <a class="text-sm text-gray-700 dark:text-gray-400 ml-2" href="{{route('home')}}">{{__('internships.filter')}}</a>
                 @endif
                 <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">

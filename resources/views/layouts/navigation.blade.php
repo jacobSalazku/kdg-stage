1d1951327eda41963bfa5c3ad4c1f3f0dd6a8ca6
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-11-21 14:11:20 +0100

    Add language dropdown to navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,11 +27,25 @@
                             </x-nav-link>
                         </div>
                     @endauth
-                    <div class="">
-                        <x-nav-link href="{{__('nav.link')}}">
-                            {{ __('nav.language') }}
-                        </x-nav-link>
-                    </div>
+                    <x-dropdown width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <svg width="20px" height="20px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="languageIconTitle" stroke="#000000" stroke-width="1" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#000000"> <title id="languageIconTitle">Language</title> <circle cx="12" cy="12" r="10"/> <path stroke-linecap="round" d="M12,22 C14.6666667,19.5757576 16,16.2424242 16,12 C16,7.75757576 14.6666667,4.42424242 12,2 C9.33333333,4.42424242 8,7.75757576 8,12 C8,16.2424242 9.33333333,19.5757576 12,22 Z"/> <path stroke-linecap="round" d="M2.5 9L21.5 9M2.5 15L21.5 15"/> </svg>
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+                        <x-slot name="content">
+                            @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                                <x-dropdown-link :href="LaravelLocalization::getLocalizedURL($localeCode, null, [], true)">
+                                    {{ $properties['native'] }}
+                                </x-dropdown-link>
+                            @endforeach
+                        </x-slot>
+                    </x-dropdown>
                     <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @auth
                     <x-dropdown align="right" width="48">

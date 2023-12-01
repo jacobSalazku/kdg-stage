7776ddbd8e28834b8a23da1400fd29002e86d936
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-12-01 15:27:28 +0100

    style(internships): improve code formatting and remove unnecessary elements

--- resources/views/internships.blade.php
+++ resources/views/internships.blade.php
@@ -1,14 +1,13 @@
 @section('pagetitle', __('internships.internships'))
 <x-app-layout>
-    <div class=" w-full  h-full bg-kdg-white flex flex-col justify-center items-center pt-10 px-6">
-        <div class="w-screen max-w-7xl flex flex-row items-center   ">
+    <div class="w-full h-full bg-kdg-white flex flex-col justify-center items-center pt-10 px-6">
+        <div class="w-screen max-w-7xl flex flex-row items-center">
             <div class="h-20 w-[87.5rem] flex flex-row border-b-2 border-kdg-light items-center px-10">
-                <div class="">
-                    <h1 class=" text-2xl font-KDG sm:text-3xl">
-                    {{__('internships.internships')}}
+                <div>
+                    <h1 class="text-2xl font-KDG sm:text-3xl">
+                        {{__('internships.internships')}}
                     </h1>
                 </div>
-
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="ml-auto bg-light-blue  border-dark-blue border focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Filter <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
@@ -27,59 +26,40 @@
                 </div>
             </div>
         </div>
-
-        <div class=" h-full  ">
-            <div class=" w-full flex flex-row justify-center gap ">
-                <div class=" w-full h-full max-w-[87.5rem] flex flex-col  items-center sm:flex-row flex-wrap sm:justify-center gap-y-8 pt-20">
-                    @foreach($companies as $company)
-                        <div class="relative flex flex-col text-gray-700 bg-white shadow-md w-80 rounded-xl border mr-4">
-                            <div class="relative  bg-white border-2 border-kdg-dark-blue flex flex-col justify-center   items-center h-32 mx-4 -mt-6 overflow-hidden  shadow-lg rounded-xl  shadow-blue-gray-500/40">
-                                <p class="text-bold text-2xl text-p-black">
-                                    {{$company->company}}
-                                </p>
+        <div class="w-full max-w-[87.5rem] flex flex-col items-center justify-center -bg-white dark:bg-gray-800 overflow-hidden mt-4 md:px-10">
+            <div class="w-full p-6 text-gray-900 dark:text-gray-100">
+                @foreach($companies as $company)
+                        <div class="h-auto mt-6 rounded border border-kdg-grey shadow-lg flex flex-col justify-center items-start px-5 py-10 dark:bg-gray-800">
+                            <h3 class="mb-1 text-3xl font-bold tracking-tight text-kdg-dark-blue ">{{$company->company}} </h3>
+                            <div class="mt-4 mb-4 text-p-black">
+                                @foreach($company->tags as $tag)
+                                    <span class="bg-{{$tag->color}}-100 text-{{$tag->color}}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-{{$tag->color}}-900 dark:text-{{$tag->color}}-300">{{$tag->name}}</span>
+                                @endforeach
                             </div>
-                            <div class="p-6">
-                                <div class="mt-4 mb-1 text-p-black">
-                                    @foreach($company->tags as $tag)
-                                        <span class="bg-{{$tag->color}}-100 text-{{$tag->color}}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-{{$tag->color}}-900 dark:text-{{$tag->color}}-300">{{$tag->name}}</span>
-                                    @endforeach
-                                </div>
-                            </div>
-                            <div class=" px-6 pb-4">
-                                <div>
-                                    <p class="font-bold"> {{__('internships.website')}}</p>
-                                    <p><a href="{{$company->website}}" class="text-decoration-line: underline" target="__blank">{{$company->website}}</a></p>
-                                </div>
-                                <div>
-                                    <p class="font-bold">{{__('internships.contact')}}</p>
-                                    <p>{{$company->contact}}</p>
-                                </div>
-                                <div>
-                                    <p class="font-bold"> {{__('internships.email')}}</p>
-                                    <p><a href="mailto:{{$company->email}}" class="text-decoration-line: underline">{{$company->email}}</a></p></div>
-                                <div>
-                                    <p class="font-bold">{{__('internships.phone')}}</p>
-                                    <p>
-                                        @if($company->phone_number)
-                                            <a href="tel:{{ $company->phone_number }}" class="text-decoration-line: underline">{{ $company->phone_number }}</a>
-                                        @else
-                                            {{ '-' }}
-                                        @endif
-                                    </p>
-                                </div>
-                            </div>
+                            <p class="font-bold"> {{__('internships.website')}}</p>
+                            <p class="font-normal text-gray-700 dark:text-gray-400"><a href="{{$company->website}}" class="text-decoration-line: underline" target="__blank">{{$company->website}}</a></p>
+                            <p class="font-bold"> {{__('internships.contact')}}</p>
+                            <p class="font-normal text-gray-700 dark:text-gray-400">{{$company->contact}}</p>
+                            <p class="font-bold"> {{__('internships.email')}}</p>
+                            <p class="font-normal text-gray-700 dark:text-gray-400"><a href="mailto:{{$company->email}}" class="text-decoration-line: underline">{{$company->email}}</a></p>
+                            <p class="font-bold"> {{__('internships.phone')}}</p>
+                            <p class="font-normal text-gray-700 dark:text-gray-400">
+                                @if($company->phone_number)
+                                    <a href="tel:{{ $company->phone_number }}" class="text-decoration-line: underline">{{ $company->phone_number }}</a>
+                                @else
+                                    {{ '-' }}
+                                @endif
+                            </p>
                         </div>
-                    @endforeach
-                </div>
+                @endforeach
+                <br>
+                @if($companies->count() == 0)
+                    <div class="h-auto mt-6 rounded border border-kdg-grey shadow-lg flex flex-col justify-center items-center px-5 py-10 gap-8 dark:bg-gray-800 dark:hover:bg-gray-70">
+                        <h4 class="text-lg font-medium tracking-tight text-deep-black ">{{__('internships.no-internships')}}</h4>
+                    </div>
+                @endif
+                {{$companies->links()}}
             </div>
-            @if($companies->count() == 0)
-                <div class="h-auto mt-6 rounded border border-kdg-grey shadow-lg flex flex-col justify-center items-center px-5 py-10 gap-8 dark:bg-gray-800 dark:hover:bg-gray-70">
-                    <h4 class="text-lg font-medium tracking-tight text-deep-black ">{{__('internships.no-internships')}}</h4>
-                </div>
-            @endif
-            <br>
-            {{$companies->links()}}
-            <br>
         </div>
     </div>
 </x-app-layout>

cc54758281fad08a4e8e5ed3861f224112c74a23
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-12-04 14:16:33 +0100

    style(login.blade.php): Add CSS class to center image on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
     <div class="flex h-full  flex-col justify-start items-center px-6 py-10 sm:px-8 bg-white">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img src="{{ asset('/img/KdG_H_Closed_Black_sRGB.png') }}" alt="KDG logo" width ="300px" height ="400px"/>
+            <img src="{{ asset('/img/KdG_H_Closed_Black_sRGB.png') }}" alt="KDG logo" width ="300px" height ="400px" class="mx-auto"/>
 
                 <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                     {{__('login.login')}}

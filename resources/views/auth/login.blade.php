04f68a95b5f24dd0078ffcc84453b8fd0d223bef
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-12-04 14:56:23 +0100

    refactor: Remove unnecessary code and update login page layout and routes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,4 @@
-@include(' layouts.navigation')
-<x-guest-layout>
-
-
+<x-app-layout>
     <div class="flex h-full  flex-col justify-start items-center px-6 py-10 sm:px-8 bg-white">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img src="{{ asset('/img/KdG_H_Closed_Black_sRGB.png') }}" alt="KDG logo" width ="300px" height ="400px" class="mx-auto"/>
@@ -10,22 +7,19 @@
                     {{__('login.login')}}
                 </h2>
             </div>
-
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form class="space-y-6">
                     <div class="w-full flex flex-col items-center justify-center gap-3">
                         <button
                             type="submit"
                             title="Google"
-                            aria-label="Login met Google"
                             class="flex w-[18rem] justify-center  mt-2 rounded-md bg-deep-black  py-4 px-3  text-kdg-white text-lg font-semibold leading-6 text-white shadow-sm hover:bg-kdg-dark-blue"
                         >
                         <a href="{{ route('oauth.login', [ 'provider' => 'google' ]) }}" class="mx-auto">{{__('login.google')}}</a>
                         </button>
                         <button
                             type="submit"
-                            title="Githuhb"
-                            aria-label="Login met Github"
+                            title="GitHub"
                             class="flex w-[18rem] justify-center  mt-2 rounded-md bg-deep-black  py-4 px-3  text-kdg-white text-lg font-semibold leading-6 text-white shadow-sm hover:bg-kdg-dark-blue"
                         >
                         <a href="{{ route('oauth.login', [ 'provider' => 'github' ]) }}" class="mx-auto">{{__('login.github')}}</a>
@@ -33,7 +27,6 @@
                         <button
                             type="submit"
                             title="Microsoft"
-                            aria-label="Login met Microsoft"
                             class="flex w-[18rem] justify-center  mt-2 rounded-md bg-deep-black  py-4 px-3  text-kdg-white text-lg font-semibold leading-6 text-white shadow-sm hover:bg-kdg-dark-blue"
                         >
                         <a href="{{ route('oauth.login', [ 'provider' => 'microsoft' ]) }}" class="mx-auto">{{__('login.microsoft')}}</a>
@@ -42,4 +35,4 @@
                 </form>
             </div>
         </div>
-</x-guest-layout>
+</x-app-layout>
--- routes/auth.php
+++ routes/auth.php
@@ -2,15 +2,18 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-});
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
+    Route::middleware('auth')->group(function () {
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
 });

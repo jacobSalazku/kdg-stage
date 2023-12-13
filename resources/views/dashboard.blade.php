a39085bd67f6f122afafdd36f392d776630bc24c
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-12-13 11:21:42 +0100

    style: Remove unnecessary class from div element

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -80,7 +80,7 @@
                     <p class="mt-4 font-light text-xs text-gray-700 dark:text-gray-400">{{__('dashboard.info')}}.</p>
                     <p class="mt-4 font-light text-xs text-red-700 dark:text-red-400">{{__('form.warning_internship')}}</p>
                     <div
-                        class="mt-4 block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        class="mt-4 block w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{Auth::user()->company}}</h5>
                         <form method="POST" action="{{route('create_internship')}}">
                             @csrf

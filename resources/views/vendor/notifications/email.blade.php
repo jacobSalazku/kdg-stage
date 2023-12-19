2b708cd7793ef494dc8a45b32022e9ea9bfb50cc
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-12-19 11:57:04 +0100

    Added back standard translations for emails

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -46,13 +46,8 @@
 {{-- Subcopy --}}
 @isset($actionText)
 <x-slot:subcopy>
-@lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
-    [
-        'actionText' => $actionText,
-    ]
-) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+    {{__('trouble', ['actionText' => $actionText])}}
+    <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
 </x-slot:subcopy>
 @endisset
 </x-mail::message>

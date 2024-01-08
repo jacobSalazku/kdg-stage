dba45e0447fc608717dac0c03391db75a1dc7277
jacobSalazku <Cooljss10@gmail.com>
2024-01-08 03:31:25 +0100

    update styling detail page vacatures

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'flex flex-row justify-center w-full text-kdg-white px-4 py-10 hover:bg-p-black  transition duration-150 ease-in-out'
-            :  ' flex flex-row justify-center w-full text-kdg-white px-4 py-10 hover:bg-p-black duration-150 ease-in-out';
+            ? 'flex flex-row justify-center w-full text-kdg-white px-4 py-10 transition duration-150 ease-in-out hover:bg-p-black'
+            :  'flex flex-row justify-center w-full text-kdg-white px-4 py-10 hover:bg-p-black duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>

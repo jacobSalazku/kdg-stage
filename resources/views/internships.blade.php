f2562fb86bdb502053da4159a7bf72b180b23ea8
jacobSalazku <Cooljss10@gmail.com>
2024-01-12 14:29:16 +0100

    fixing grid in job page

--- resources/views/internships.blade.php
+++ resources/views/internships.blade.php
@@ -1,14 +1,14 @@
 @section('pagetitle', __('internships.internships'))
 <x-app-layout>
-    <div class="w-full h-full bg-kdg-white flex flex-col justify-center items-center pt-10 px-6">
-        <div class="w-screen max-w-7xl flex flex-row items-center mb-6">
+    <div class="flex flex-col items-center justify-center w-full h-full px-6 pt-10 bg-kdg-white">
+        <div class="flex flex-row items-center w-screen mb-6 max-w-7xl">
             <div class="h-20 w-[87.5rem] flex flex-row border-b-2 border-kdg-light items-center px-10">
                 <div>
                     <h1 class="text-2xl font-KDG sm:text-3xl">
                         {{__('internships.internships')}}
                     </h1>
                 </div>
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="ml-auto bg-light-blue  border-dark-blue border focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Filter <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="ml-auto bg-light-blue border-dark-blue border focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Filter <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
                 </button>
@@ -24,7 +24,7 @@
             </div>
         </div>
         <div class="w-full max-w-[87.5rem] flex flex-col items-center justify-center -bg-white dark:bg-gray-800 overflow-hidden mt-4 md:px-10">
-            <div class="w-full flex justify-between col-span-3 px-6">
+            <div class="flex justify-between w-full col-span-3 px-6">
                 @if ($filtered === 1)
                     <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">
                         {{count($companies)}}
@@ -37,9 +37,9 @@
                     <a class="hover:underline" href="{{route('home')}}">{{__('internships.back')}}</a>
                 @endif
             </div>
-            <div class="w-full p-6 text-gray-900 dark:text-gray-100 md:grid grid-cols-3 gap-2 justify-evenly">
+            <div class="w-full grid-cols-3 gap-2 p-6 text-gray-900 dark:text-gray-100 md:grid justify-evenly">
                 @foreach($companies as $company)
-                        <div class="h-auto rounded border border-kdg-grey shadow-lg flex flex-col justify-center items-start px-5 py-10 dark:bg-gray-800 mb-2">
+                        <div class="flex flex-col items-start justify-center h-auto px-5 py-10 mb-2 border rounded shadow-lg border-kdg-grey dark:bg-gray-800">
                             <h3 class="mb-1 text-3xl font-bold tracking-tight text-kdg-dark-blue ">{{$company->company}} </h3>
                             <div class="mt-4 mb-4 text-p-black">
                                 @foreach($company->tags as $tag)
@@ -47,15 +47,15 @@
                                 @endforeach
                             </div>
                             <p class="font-bold"> {{__('internships.website')}}</p>
-                            <p class="font-normal text-gray-700 dark:text-gray-400"><a href="{{$company->website}}" class="text-decoration-line: underline" target="__blank">{{$company->website}}</a></p>
+                            <p class="font-normal text-gray-700 dark:text-gray-400"><a href="{{$company->website}}" class="underline text-decoration-line:" target="__blank">{{$company->website}}</a></p>
                             <p class="font-bold"> {{__('internships.contact')}}</p>
                             <p class="font-normal text-gray-700 dark:text-gray-400">{{$company->contact}}</p>
                             <p class="font-bold"> {{__('internships.email')}}</p>
-                            <p class="font-normal text-gray-700 dark:text-gray-400"><a href="mailto:{{$company->email}}" class="text-decoration-line: underline">{{$company->email}}</a></p>
+                            <p class="font-normal text-gray-700 dark:text-gray-400"><a href="mailto:{{$company->email}}" class="underline text-decoration-line:">{{$company->email}}</a></p>
                             <p class="font-bold"> {{__('internships.phone')}}</p>
                             <p class="font-normal text-gray-700 dark:text-gray-400">
                                 @if($company->phone_number)
-                                    <a href="tel:{{ $company->phone_number }}" class="text-decoration-line: underline">{{ $company->phone_number }}</a>
+                                    <a href="tel:{{ $company->phone_number }}" class="underline text-decoration-line:">{{ $company->phone_number }}</a>
                                 @else
                                     {{ '-' }}
                                 @endif
@@ -64,7 +64,7 @@
                 @endforeach
                 <br>
                 @if($companies->count() === 0 && !request()->has('filter'))
-                    <div class="h-auto rounded border border-kdg-grey shadow-lg px-5 py-5 text-center">
+                    <div class="h-auto px-5 py-5 text-center border rounded shadow-lg border-kdg-grey">
                         <h4 class="text-lg font-medium tracking-tight text-deep-black">{{__('internships.no-internships')}}</h4>
                     </div>
                 @endif
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,7 +1,7 @@
 @section('pagetitle', __('jobs.jobs'))
 <x-app-layout>
-    <div class="w-full h-full bg-kdg-white flex flex-col justify-center items-center pt-10 px-6">
-        <div class="w-screen max-w-7xl flex flex-row items-center">
+    <div class="flex flex-col items-center justify-center w-full h-full px-6 pt-10 bg-kdg-white">
+        <div class="flex flex-row items-center w-screen max-w-7xl">
             <div class="h-20 w-[87.5rem] flex flex-row border-b-2 border-kdg-light items-center px-10">
                 <div>
                     <h1 class="text-2xl font-KDG sm:text-3xl">
@@ -10,8 +10,8 @@
                 </div>
                 <form action="{{route('search')}}" method="get" class="ml-auto">
                     <div class="flex flex-row items-center">
-                        <input type="text" name="query" id="query" value='{{$search ?? ''}}' placeholder="{{__('jobs.find')}}" class="block w-full sm:w-64 py-2 pl-10 pr-4 leading-5 rounded-full bg-gray-100 dark:bg-gray-700 border-transparent focus:outline-none focus:bg-white focus:border-gray-300 dark:focus:border-gray-500 focus:ring focus:ring-gray-200 dark:focus:ring-gray-400 transition duration-150 ease-in-out">
-                        <button type="submit" class="w-full ml-2 px-3 py-2 rounded-full bg-deep-black text-kdg-white text-bold hover:bg-kdg-white hover:text-deep-black hover:border-deep-black border transition duration-150 ease-in-out">
+                        <input type="text" name="query" id="query" value='{{$search ?? ''}}' placeholder="{{__('jobs.find')}}" class="block w-full py-2 pl-10 pr-4 leading-5 transition duration-150 ease-in-out bg-gray-100 border-transparent rounded-full sm:w-64 dark:bg-gray-700 focus:outline-none focus:bg-white focus:border-gray-300 dark:focus:border-gray-500 focus:ring focus:ring-gray-200 dark:focus:ring-gray-400">
+                        <button type="submit" class="w-full px-3 py-2 ml-2 transition duration-150 ease-in-out border rounded-full bg-deep-black text-kdg-white text-bold hover:bg-kdg-white hover:text-deep-black hover:border-deep-black">
                             {{__('jobs.search')}}
                         </button>
                         @if($filtered === 1)
@@ -21,7 +21,7 @@
             </div>
         </div>
         <div class="w-full max-w-[87.5rem] flex flex-col items-center justify-center -bg-white dark:bg-gray-800 overflow-hidden mt-4 md:px-10">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="flex flex-col w-full p-6 text-gray-900 dark:text-gray-100">
                 <div class="flex justify-between">
                     @if ($filtered === 1)
                         <h5 class="text-xl font-semibold leading-none text-gray-800 dark:text-gray-200">
@@ -35,9 +35,9 @@
                         <a class="hover:underline" href="{{route('jobs')}}">{{__('jobs.back')}}</a>
                     @endif
                 </div>
-                <div class="w-full max-w-[87.5rem] md:grid grid-cols-2 gap-6 mt-6">
+                <div class="flex flex-row flex-wrap gap-6 mt-6 transition-transform sm:grid sm:grid-cols-2 ">
                     @foreach($jobs as $job)
-                        <a href="{{route('detail', ['id' => $job->id])}}" class="rounded border border-kdg-grey shadow-lg px-5 py-10 gap-8 cursor-pointer hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-70">
+                        <a href="{{route('detail', ['id' => $job->id])}}" class="h-full gap-8 px-5 py-10 border rounded shadow-lg cursor-pointer border-kdg-grey hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-70">
                             <h3 class="mb-1 text-3xl font-bold tracking-tight text-kdg-dark-blue ">{{$job->title}} </h3>
                             <h4 class="mb-1 text-lg font-medium tracking-tight text-deep-black ">{{$job->company}}</h4>
                             <p class="font-normal text-gray-700 dark:text-gray-400">{{ substr(strip_tags($job->description), 0, 250) }} ...</p>
@@ -46,7 +46,7 @@
                     @endforeach
                 </div>
                 @if($jobs->count() === 0 && $filtered === 0)
-                    <div class="h-auto rounded border border-kdg-grey shadow-lg px-5 py-5 text-center">
+                    <div class="h-auto px-5 py-5 text-center border rounded shadow-lg border-kdg-grey">
                         <h4 class="text-lg font-medium tracking-tight text-deep-black ">{{__('jobs.no-jobs')}}</h4>
                     </div>
                 @endif
